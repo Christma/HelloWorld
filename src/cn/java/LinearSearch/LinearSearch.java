2565f4f4bbcb8ef6f3b6b5c41b50344b23a6f24f
liuchuanmu <liuchuanmumumu@aliyun.com>
2020-07-15 11:47:22 +0800

    LinearSearch

--- src/cn/java/LinearSearch/LinearSearch.java
+++ src/cn/java/LinearSearch/LinearSearch.java
@@ -2,7 +2,8 @@ package cn.java.LinearSearch;
 
 public class LinearSearch {
 
-    public int search(int[] data, int target) {
+    private LinearSearch(){}
+    public static int search(int[] data, int target) {
         for (int i = 0; i < data.length; i++) {
             if (data[i] == target) {
                 return i;
@@ -11,8 +12,5 @@ public class LinearSearch {
         return -1;
     }
 
-    public static void main(String[] args) {
-        int[] datas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-        System.out.println(new LinearSearch().search(datas, 6));
-    }
+
 }
---
+++ src/cn/java/LinearSearch/Main.java
@@ -0,0 +1,8 @@
+package cn.java.LinearSearch;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] datas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        System.out.println(LinearSearch .search(datas, 6));
+    }
+}
