8a883b625aaa62b9ecac972458831ecb2d22f1fa
liuchuanmu <liuchuanmumumu@aliyun.com>
2020-07-15 11:40:52 +0800

    Add Main

--- src/cn/java/LinearSearch/LinearSearch.java
+++ src/cn/java/LinearSearch/LinearSearch.java
@@ -10,4 +10,9 @@ public class LinearSearch {
         }
         return -1;
     }
+
+    public static void main(String[] args) {
+        int[] datas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        System.out.println(new LinearSearch().search(datas, 6));
+    }
 }
