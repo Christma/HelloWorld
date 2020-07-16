dd73a79a206c215fefeec7c3333315efbbeb04ec
liuchuanmu <liuchuanmumumu@aliyun.com>
2020-07-16 20:36:36 +0800

    LinearSearch

--- src/cn/java/LinearSearch/Main.java
+++ src/cn/java/LinearSearch/Main.java
@@ -3,6 +3,12 @@ package cn.java.LinearSearch;
 public class Main {
     public static void main(String[] args) {
         Integer[] datas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-        System.out.println(LinearSearch .search(datas, 6));
+        System.out.println(LinearSearch.search(datas, 6));
+
+        Student[] students = {new Student("chuanmu"), new Student("Bobo"), new Student("Alice")};
+        Student student = new Student("chuanmu");
+
+        System.out.println(LinearSearch.search(students, student));
+
     }
 }
