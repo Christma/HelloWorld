614c3a5c38945f50fa0ffdca82ebf826c0f51b13
liuchuanmu <liuchuanmumumu@aliyun.com>
2020-07-16 20:11:21 +0800

    LinearSearch

--- src/cn/java/LinearSearch/Main.java
+++ src/cn/java/LinearSearch/Main.java
@@ -2,7 +2,7 @@ package cn.java.LinearSearch;
 
 public class Main {
     public static void main(String[] args) {
-        int[] datas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        Integer[] datas = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
         System.out.println(LinearSearch .search(datas, 6));
     }
 }
