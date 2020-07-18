1e53bc8716918217a8a0b4332429569882cec618
christma <liuchuanmumumu@gmail.com>
2020-07-18 16:54:48 +0800

    InsertSort

--- src/cn/java/InsertSort/InsertSort.java
+++ src/cn/java/InsertSort/InsertSort.java
@@ -34,6 +34,17 @@ public class InsertSort {
         }
     }
 
+    public static <E extends Comparable<E>> void sortPre2(E[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            E e = arr[i];
+            int j;
+            for (j = i; j < arr.length - 1 && e.compareTo(arr[j + 1]) > 0; j++) {
+                arr[j] = arr[j + 1];
+            }
+            arr[j] = e;
+        }
+    }
+
 
     private static <E> void swap(E[] arr, int i, int j) {
         E temp = arr[i];
@@ -42,12 +53,14 @@ public class InsertSort {
     }
 
     public static void main(String[] args) {
-        int[] nums = {10000, 100000};
+        int[] nums = {10000};
         for (int n : nums) {
             Integer[] data = ArrayGenerator.generatorRandomArray(n, n);
             Integer[] data2 = Arrays.copyOf(data, n);
             SortingHelper.testSorted("InsertSort", data);
             SortingHelper.testSorted("InsertSort2", data2);
+            SortingHelper.testSorted("InsertSortPre2", data2);
+
         }
     }
 }
