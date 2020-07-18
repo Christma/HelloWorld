e6477764f912cfbc42a57dc6d46d1cbc21b38da6
christma <liuchuanmumumu@gmail.com>
2020-07-18 15:41:29 +0800

    SelectionSort

--- src/cn/java/SelectionSort/SelectionSort.java
+++ src/cn/java/SelectionSort/SelectionSort.java
@@ -18,15 +18,35 @@ public class SelectionSort {
         }
     }
 
+    public static <E extends Comparable<E>> void preOrder(E[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int maxIndex = i;
+            for (int j = i; j >= 0; j--) {
+                if (arr[j].compareTo(arr[maxIndex]) > 0) {
+                    maxIndex = j;
+                }
+            }
+            swap(arr, i, maxIndex);
+        }
+    }
+
+
     private static <E> void swap(E[] arr, int i, int j) {
         E temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
+
     public static void main(String[] args) {
-        int n = 10000;
-        Integer[] data = ArrayGenerator.generatorRandomArray(n, n);
-        SortingHelper.testSorted("SelectionSort", data);
+        int[] nums = {10000, 100000};
+        for (int n : nums) {
+            Integer[] data = ArrayGenerator.generatorRandomArray(n, n);
+            SortingHelper.testSorted("SelectionSort", data);
+        }
+        for (int n : nums) {
+            Integer[] data = ArrayGenerator.generatorRandomArray(n, n);
+            SortingHelper.testSorted("SelectionSortpre", data);
+        }
     }
 }
