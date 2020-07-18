b3e7b2c93bfc6098b1d4449354e6ab209d1ad2c6
christma <liuchuanmumumu@gmail.com>
2020-07-18 08:03:09 +0800

    LinearSearch

--- src/cn/java/SelectionSort/SelectionSort.java
+++ src/cn/java/SelectionSort/SelectionSort.java
@@ -5,12 +5,12 @@ public class SelectionSort {
     private SelectionSort() {
     }
 
-    public static void sort(int[] arr) {
+    public static <E extends Comparable<E>> void sort(E[] arr) {
 
         for (int i = 0; i < arr.length; i++) {
             int minIndex = i;
             for (int j = i; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) {
+                if (arr[j].compareTo(arr[minIndex]) < 0) {
                     minIndex = j;
                 }
             }
@@ -18,17 +18,18 @@ public class SelectionSort {
         }
     }
 
-    private static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
+    private static <E> void swap(E[] arr, int i, int j) {
+        E temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
     public static void main(String[] args) {
-        int[] data = {1, 4, 2, 3, 5};
-        SelectionSort.sort(data);
-        for (int i = 0; i < data.length; i++) {
-            System.out.println(data[i]);
+        Integer[] data = {1, 4, 2, 3, 5};
+        String[] str = {"a","z","t","b","q"};
+        SelectionSort.sort(str);
+        for (int i = 0; i < str.length; i++) {
+            System.out.println(str[i]);
         }
     }
 }
