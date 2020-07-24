3c10250b81a000da251b22a91f0d2b8e8841d303
christma <liuchuanmumumu@gmail.com>
2020-07-25 07:58:58 +0800

    InsertSort

---
+++ src/cn/java/Array/Array.java
@@ -0,0 +1,12 @@
+package cn.java.Array;
+
+public class Array {
+    private int[] data;
+    private int size;
+
+    public void Array(int capacity){
+        data = new int[capacity];
+        size =0;
+    }
+
+}
