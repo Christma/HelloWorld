27de169be2630b3bf8e4a86d840a9bebafe8a8f3
christma <liuchuanmumumu@gmail.com>
2020-07-26 11:44:38 +0800

    Array

--- src/cn/java/Array/Array.java
+++ src/cn/java/Array/Array.java
@@ -4,9 +4,49 @@ public class Array {
     private int[] data;
     private int size;
 
-    public void Array(int capacity){
+    public Array(int capacity) {
         data = new int[capacity];
-        size =0;
+        size = 0;
+    }
+
+    public Array() {
+        this(10);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return data.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addLast(int e) {
+        add(size, e);
+    }
+
+    public void addFirst(int e) {
+        add(0, e);
+    }
+
+
+    public void add(int index, int e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("index error");
+        }
+        if (size == getCapacity()) {
+            throw new IllegalArgumentException("array is full");
+        }
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+
     }
 
 }
