2bc36dd862cb9f2a70ee1a56ce50af4539e5d48e
christma <liuchuanmumumu@gmail.com>
2020-08-03 22:14:19 +0800

    Array

--- src/cn/java/Array/Array.java
+++ src/cn/java/Array/Array.java
@@ -79,7 +79,7 @@ public class Array<E> {
     }
 
     private void resize(int newCapacity) {
-        E[] newData = (E[]) new Objects[newCapacity];
+        E[] newData = (E[]) new Object[newCapacity];
         for (int i = 0; i < size; i++) {
             newData[i] = data[i];
         }
