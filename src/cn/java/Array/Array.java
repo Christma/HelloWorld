fad157ecd4596541b27caf713bccc39dfa2413f5
christma <liuchuanmumumu@gmail.com>
2020-08-04 21:50:42 +0800

    Array

--- src/cn/java/Array/Array.java
+++ src/cn/java/Array/Array.java
@@ -127,7 +127,7 @@ public class Array<E> {
         size--;
         data[size] = null;
 
-        if (size < data.length / 2) {
+        if (size < data.length / 4) {
             resize(data.length / 2);
         }
         return temp;
