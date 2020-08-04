1b15a40212d299f88282c08593c1b76f9daca0d2
christma <liuchuanmumumu@gmail.com>
2020-08-04 21:33:15 +0800

    Array

--- src/cn/java/Array/Main.java
+++ src/cn/java/Array/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Array<Integer> arr = new Array();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             arr.addLast(i);
         }
         System.out.println(arr);
