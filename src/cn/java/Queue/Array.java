4a828c386ce30d26aa4c286904484ebc08fb8863
christma <liuchuanmumumu@gmail.com>
2020-08-09 14:07:45 +0800

    Queue

---
+++ src/cn/java/Queue/Array.java
@@ -0,0 +1,166 @@
+package cn.java.Queue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Array<E> {
+    private E[] data;
+    private int size;
+
+    public Array(int capacity) {
+        data = (E[]) new Object[capacity];
+        size = 0;
+    }
+
+    public Array() {
+        this(10);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCapacity() {
+        return data.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("index error");
+        }
+        return data[index];
+    }
+
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("index error");
+        }
+        data[index] = e;
+    }
+
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("index error");
+        }
+        if (size == getCapacity()) {
+            resize(2 * data.length);
+        }
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+
+    }
+
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i] == e) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public List<Integer> finds(E e) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+    public boolean removeElements(E e) {
+        List<Integer> lists = new ArrayList<>();
+        lists = finds(e);
+        for (Integer i : lists) {
+            remove(i);
+        }
+        return true;
+    }
+
+
+    public E remove(int index) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("index error");
+        }
+        E temp = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        size--;
+        data[size] = null;
+
+        if (size < data.length / 4 && data.length / 2 != 0) {
+            resize(data.length / 2);
+        }
+        return temp;
+    }
+
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    public void removeElement(E e) {
+        int i = find(e);
+        if (i != -1) {
+            remove(i);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Array{" +
+                "data=" + Arrays.toString(data) +
+                ", size=" + size +
+                ", capacity=" + getCapacity() +
+                '}';
+    }
+}
