a23a62cd94b2ca701c7119acc9dd920169739061
christma <liuchuanmumumu@gmail.com>
2020-08-17 22:00:50 +0800

    LinkedList

--- src/cn/java/LinkedList/LinkedList.java
+++ src/cn/java/LinkedList/LinkedList.java
@@ -25,4 +25,51 @@ public class LinkedList<E> {
             return e.toString();
         }
     }
+
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addFirst(E e) {
+        head = new Node(e, head);
+        size++;
+    }
+
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException(" index is error ");
+        }
+
+        if (index == 0) {
+            addFirst(e);
+        } else {
+            Node pre = head;
+            for (int i = 0; i < index - 1; i++) {
+                pre = pre.next;
+            }
+
+            Node node = new Node(e);
+            node.next = pre.next;
+            pre.next = node;
+            size++;
+        }
+
+    }
+
+    public void addLast(E e) {
+        add(size, e);
+    }
+
 }
