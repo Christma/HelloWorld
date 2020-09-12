0be4710710be9cd31278dd88d04efa019de3ec46
christma <liuchuanmumumu@gmail.com>
2020-09-12 19:06:59 +0800

    Add method

--- src/cn/java/LinkedList/Sum.java
+++ src/cn/java/LinkedList/Sum.java
@@ -1,14 +1,19 @@
 package cn.java.LinkedList;
 
 public class Sum {
-    public static int sum(int [] arr){
-        return sum(arr,0);
+    public static int sum(int[] arr) {
+        return sum(arr, 0);
     }
 
     private static int sum(int[] arr, int i) {
-        if(i == arr.length){
+        if (i == arr.length) {
             return 0;
         }
-        return arr[i] + sum(arr,i+1);
+        return arr[i] + sum(arr, i + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(sum(arr));
     }
 }
