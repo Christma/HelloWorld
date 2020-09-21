734ec5744022aee0b0fa6d1acef53858de036a07
christma <liuchuanmumumu@gmail.com>
2020-09-21 22:09:13 +0800

    MergeSort

---
+++ src/cn/java/MergeSort/MergeSort.java
@@ -0,0 +1,38 @@
+package cn.java.MergeSort;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    private MergeSort() {
+
+    }
+
+    public static <E extends Comparable<E>> void sort(E[] arr) {
+
+    }
+
+
+    private static <E extends Comparable<E>> void merge(E[] arr, int l, int mid, int r) {
+        E[] temp = Arrays.copyOfRange(arr, l, r + 1);
+
+        int i = l;
+        int j = mid + 1;
+
+        for (int k = 0; k <= r; k++) {
+            if (i > mid) {
+                arr[k] = temp[j - l];
+                j++;
+            } else if (j > r) {
+                arr[k] = temp[i - l];
+                i++;
+            } else if (temp[i - l].compareTo(temp[j - l]) <= 0) {
+                arr[k] = temp[i - l];
+                i++;
+            } else {
+                arr[k] = temp[j - l];
+                j++;
+            }
+        }
+    }
+}
